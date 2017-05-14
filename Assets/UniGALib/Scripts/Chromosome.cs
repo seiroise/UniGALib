257f692f35976a64bfa3fbf774378dfe8c5e6035
seiroise <seiroise@gmail.com>
2017-05-14 21:21:01 +0900

    first commit

---
+++ Assets/UniGALib/Scripts/Chromosome.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UniGALib {
+
+	/// <summary>
+	/// 染色体
+	/// </summary>
+	public class Chromosome<T> {
+
+		private T[] _genes;
+
+		public Chromosome(int count) {
+			_genes = new T[count];
+		}
+	}
+}
---
+++ Assets/UniGALib/Scripts/Individual.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace UniGALib {
+
+	/// <summary>
+	/// 染色体を持つ個体
+	/// </summary>
+	public class Individual<T> {
+
+		private Chromosome<T>[] _chromosomes;
+	}
+}
